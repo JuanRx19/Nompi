07d8422a6b7483d84d3f61a8c45298e3ab89976c
JuanRx19 <juanrx1904@gmail.com>
2026-01-13 06:00:49 -0500

    Ultimo ajuste para deploy

--- backend-nompi/src/main.ts
+++ backend-nompi/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Allow frontend running on Vite dev server
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: [process.env.FRONT_END_URL || 'http://localhost:5173'],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
