cf637ec7dd0c7ce00a83725fe9109c8b8110ab7b
JuanRx19 <juanrx1904@gmail.com>
2026-01-13 05:53:03 -0500

    Ajustes en el entrypoint para el deploy.

--- backend-nompi/docker/entrypoint.sh
+++ backend-nompi/docker/entrypoint.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env sh
 set -e
 
-echo "�🚀 Aplicando migraciones..."
+echo "🚀 Aplicando migraciones..."
 npx prisma migrate deploy
 
-echo "📦 Insertando productos..."
-export PGPASSWORD=1234
-psql -h postgres -U postgres -d nompi_db -f /backend-nompi/prisma/init-scripts/init-products.sql || true
+echo "📦 Insertando productos (SQL)..."
+psql "$DATABASE_URL" -f /backend-nompi/prisma/init-scripts/init-products.sql || true
 
 echo "🏁 Iniciando aplicación Nest..."
 npm run start:prod
